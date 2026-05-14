00c9f40755e0743c1a45bbe44cfca9ba16a2e872
Harshittiwari73 <112146773+Harshittiwari73@users.noreply.github.com>
2026-05-14 14:06:54 +0530

    fixed render crash

--- Program.cs
+++ Program.cs
@@ -5,34 +5,14 @@ using EmployeeLeaveManagement.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using EmployeeLeaveManagement.Filters;
-using EmployeeLeaveManagement.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-#region Database
-
 builder.Services.AddDbContext<EmployeeLeaveManagementContext>(options =>
-{
-    if (builder.Environment.IsDevelopment())
-    {
-        // Local SQL Server
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection"));
-    }
-    else
-    {
-        // Live Supabase PostgreSQL
-        options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
-    }
-});
-
-#endregion
-
-#region JWT
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
@@ -50,54 +30,22 @@ builder.Services.AddAuthentication(options =>
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuerSigningKey = true,
-
         IssuerSigningKey = new SymmetricSecurityKey(key),
-
         ValidateIssuer = true,
         ValidIssuer = jwtSettings["Issuer"],
-
         ValidateAudience = true,
         ValidAudience = jwtSettings["Audience"],
-
         ValidateLifetime = true,
         ClockSkew = TimeSpan.Zero
     };
-
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            context.Token = context.Request.Cookies["jwtToken"];
-            return Task.CompletedTask;
-        },
-
-        OnChallenge = context =>
-        {
-            context.HandleResponse();
-            context.Response.Redirect("/Account/Login");
-            return Task.CompletedTask;
-        }
-    };
 });
-
-#endregion
-
-#region Dependency Injection
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 builder.Services.AddScoped<IPermissionService, PermissionService>();
 
-#endregion
-
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 
@@ -106,12 +54,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
-app.UseAuthorization();
 
-//DbInitializer.Initialize(app.Services);
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
